cfc378867fb239b1610f396fabaaa11da3acde4a
normanvo <normanvo@students.cs.ubc.ca>
2023-11-06 13:02:59 -0800

    Add sql script

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,29 @@
 require('dotenv').config();
 const mysql = require('mysql2');
+const fs = require('fs');
 
-const connection = mysql.createConnection(`mysql://${process.env.usern}:${process.env.password}@${process.env.host}:${process.env.port}/${process.env.database}`);
+const queries = fs.readFileSync("./CPSC304Script.sql", {encoding: 'utf8'}).toString();
 
-connection.connect((err) => {
+
+const connection = mysql.createConnection(`mysql://${process.env.usern}:${process.env.password}@${process.env.host}:${process.env.port}/${process.env.database}`, {multipleStatements: true});
+
+connection.connect(async (err) => {
     if (err) {
         console.log('Connection error message: ' + err.message);
         return;
     }
     console.log('Connected!');
+    
+    runScript();
 });
-   
-connection.end();
+
+const runScript = async () => {
+    const queryResult = await connection.promise().query(queries).then((err, result) => {
+        if (err) {
+            console.log("Query error: " + err.message);
+            return;
+        }
+        console.log(result);
+    })
+}
+// connection.end();
