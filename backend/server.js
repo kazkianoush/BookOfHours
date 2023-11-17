a10930db38d8fbd9ad4d96a8ac6ed503af7ba34b
dngo08 <danhngo80@gmail.com>
2023-11-17 03:34:48 -0800

    Added routers and controller methods for Book

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express'); 
 const memoryRouter = require('./router/memoryRouter.js');
+const bookRouter = require('./router/bookRouter.js');
 const database = require('./utils/database.js')
 
 const app = express();
@@ -11,7 +12,7 @@ const server = app.listen(port, () => {
 });
 
 app.use('/memory', memoryRouter);
-// app.use('/book', bookRouter);
+app.use('/book', bookRouter);
 
 process.on('exit', () => {database.disconnect()})
 
