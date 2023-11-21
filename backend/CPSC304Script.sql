0fff055c21e96491b9de28fb781cf947e62b5186
normanvo <normanvo@students.cs.ubc.ca>
2023-11-20 16:38:45 -0800

    Add memory sql statements

--- backend/CPSC304Script.sql
+++ backend/CPSC304Script.sql
@@ -308,6 +308,8 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 -- Adding Data
 -- -----------------------------------------------------
+
+-- MEMORY --
 INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
 VALUES ('ME001', 'Memory: Taste', 'Considering sustenance and beverages', 0, 0, 0, 0);
 
@@ -315,13 +317,187 @@ INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, me
 VALUES ('ME002', 'Memory: Sound', 'Considering the Hush House Key', 0, 0, 0, 0);
 
 INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME003', 'Memory: Sight', 'Considering Busts, burning things', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME004', 'Memory: Scent', 'Considering flowers', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME005', 'Memory: Touch', 'Considering comforts', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME006', 'Memory: Contradiction', 'Edge books', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
 VALUES ('ME007', 'Memory: Foresight', 'Edge or Forge books', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME008', 'Memory: Satisfaction', 'Grail books', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME009', 'Memory: Solace', 'Heart or Sky books, or Dog / Cat pets', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME010', 'Memory: Storm', 'Heart books', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME011', 'Memory: Salt', 'Moon books', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME012', 'Memory: Pattern', 'Knock books', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME013', 'Memory: Revelation', 'Lantern books', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME014', 'Memory: A Stolen Secret', 'Knock or Moon books', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME015', 'Memory: Intuition', 'Rose or Moon books', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME016', 'Memory: Impulse', 'Moth books', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME017', 'Memory: Gossip', 'Various books, or Cockatoo pet', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME018', 'Memory: Fear', 'Scale or Edge books', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME019', 'Memory: Regret', 'Winter books', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME020', 'Memory: Hindsight', 'Winter books', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME021', 'Secret Threshold', 'Knock books, or Knock 5 crafts', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME022', 'Confounding Parable', 'Rose, Sky or Moon 5 crafts and books', 0, 0, 0, 0);
 
 INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
 VALUES ('ME023', 'Beguiling Melody', 'Grail books or Grail 5 craft', 1, 0, 0, 0);
 
 INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME024', 'Cheerful Ditty', 'Sky books, or Heart 5 crafts', 1, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME025', 'Savage Hymm', 'Singlefoot Songs, Unhatched Hymms (books) or Scale/Nectar 5 crafts', 1, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME026', 'Wistful Air', 'Sky 5 crafts', 1, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME027', 'Bittersweet Uncertainty', 'Various books, or Winter 5 crafts', 1, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME028', 'Earth-Sign', 'Rapt in the King, De Ratio Quercum (books), or Viper pets', 0, 1, 1, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME029', 'Occult Scraps', 'Considering Yellowing Newspaper', 0, 0, 1, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME030', 'Old Wound', 'Edicts Martial Moon 5 craft', 0, 0, 1, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME031', 'Winning Move', 'Edge 10 crafts', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME032', 'Curious Hunch', 'Knock 10 crafts', 0, 1, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME033', 'Enduring Reflection', 'Rose 10 crafts', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME034', 'Old Moment', 'Scale crafting, or Living Relic pet', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME035', 'Hive''s Lament', 'Gather at Beehive during Numa', 1, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME036', 'Wind-Rumour', 'Sky craft, or Seagull pet', 0, 1, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME037', 'Ascendant Harmony', 'Sky 10 crafts', 1, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME038', 'Didumos', 'Sky 15 crafts', 0, 0, 1, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME039', 'Horizon-Sight', 'Stymphling pet, or craft Rose 15', 0, 0, 1, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME040', 'Forbidden Epic', 'Edge 15 crafts', 0, 0, 1, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME041', 'Invincible Audacity', 'Edge 15 crafts', 0, 0, 1, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
 VALUES ('ME042', 'Earthquake Name', 'Scale 15 crafts', 0, 1, 1, 0);
 
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME043', 'Numen: Loopholes', 'Serpent-Root', 0, 0, 0, 0);
 
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME044', 'Numen: Inescapable Confinement', 'Towards A Fundamental Aesthetic', 0, 0, 0, 0);
 
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME045', 'Numen: The Sun''s Weakness', 'The Sun Disfigured', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME046', 'Numen: A Final Understanding', 'Towards A Fundamental Aesthetic: Second Edition', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME047', 'Numen: That Old Lost Music', 'The Turquoise Hand', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME048', 'Numen: The Great Counterfeit', 'Shuritic Book of Suns', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME049', 'Numen: Merciless Alterations', 'A Child''s Treasure of Golden Afternoons', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME050', 'Numen: The Bells of Ys', 'Amiranis Beteli', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME051', 'Numen: Back Into Balance', 'The Sky in the Scar', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME052', 'Numen: Weaving The World', 'Kopralith Omphalos', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME053', 'Numen: The Paths of the Sun', 'The Sun''s Design', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME054', 'Numen: An Irresistible Feast', 'The Writing On The Wall', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME055', 'Numen: Three Rules', 'The Three And The Three (St Chiavi Manuscript)', 0, 0, 0, 0);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME056', 'Clouds', 'Random draw on new day, most likely in Spring and Summer', 0, 0, 0, 1);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME057', 'Sunny', 'Random draw on new day, most likely in Spring and Summer', 0, 0, 0, 1);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME058', 'Rain', 'Random draw on new day, most likely in Fall', 0, 0, 0, 1);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME059', 'Gale', 'Random draw on new day, most likely in Fall and Winter', 0, 0, 0, 1);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME060', 'Fog', 'Random draw on new day, most likely in Fall', 0, 0, 0, 1);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME061', 'Hail', 'Random draw on new day, most likely in Winter', 0, 0, 0, 1);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME062', 'Storm', 'Random draw on new day, most likely in Winter', 0, 0, 0, 1);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME063', 'Snow', 'Random draw on new day, only in Winter', 0, 0, 0, 1);
+
+INSERT IGNORE INTO Memory(memoryID, memoryName, memorySources, memoryIsSound, memoryIsOmen, memoryIsPersistent, memoryIsWeather)
+VALUES ('ME064', 'Nume-Brume', 'Guaranteed in Numa', 0, 0, 0, 1);
